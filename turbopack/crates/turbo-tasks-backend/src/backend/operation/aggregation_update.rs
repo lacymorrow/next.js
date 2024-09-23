61ebc941187ec196f1f337a9ef6cbbf7c3aacc1d
Tobias Koppers <tobias.koppers@googlemail.com>
2024-09-23 12:19:12 +0200

    Revert "WIP: logging"
    This reverts commit c7dde65b47f09758a45307d5400fe03d442aae94.

--- turbopack/crates/turbo-tasks-backend/src/backend/operation/aggregation_update.rs
+++ turbopack/crates/turbo-tasks-backend/src/backend/operation/aggregation_update.rs
@@ -170,7 +170,6 @@ impl AggregatedDataUpdate {
             dirty_container_update,
             collectibles_update,
         } = self;
-        println!("apply {:?} {dirty_container_update:?}", task.id());
         let mut result = Self::default();
         if let Some((dirty_container_id, count)) = dirty_container_update {
             let mut added = false;
@@ -220,19 +219,11 @@ impl AggregatedDataUpdate {
                 );
                 (new != 0).then_some(new)
             });
-            println!(
-                "apply {task_id:?} {dirty_container_update:?} added={added:?} removed={removed:?} \
-                 count_update={count_update} => {:?}",
-                result.dirty_container_update
-            );
             if let Some((_, count)) = result.dirty_container_update.as_ref() {
                 if *count < 0 {
                     if let Some(root_state) = get!(task, AggregateRoot) {
                         root_state.all_clean_event.notify(usize::MAX);
                         if matches!(root_state.ty, ActiveType::CachedActiveUntilClean) {
-                            println!(
-                                "apply {task_id:?} {dirty_container_update:?} removed RootState"
-                            );
                             task.remove(&CachedDataItemKey::AggregateRoot {});
                         }
                     }
@@ -326,7 +317,6 @@ impl AggregationUpdateQueue {
     }
 
     pub fn push(&mut self, job: AggregationUpdateJob) {
-        println!("push {:?}", job);
         self.jobs.push_back(job);
     }
 
@@ -343,7 +333,6 @@ impl AggregationUpdateQueue {
     pub fn process(&mut self, ctx: &mut ExecuteContext<'_>) -> bool {
         self.processed_jobs += 1;
         if let Some(job) = self.jobs.pop_front() {
-            println!("process {:?}", job);
             match job {
                 AggregationUpdateJob::UpdateAggregationNumber {
                     task_id,
@@ -470,12 +459,7 @@ impl AggregationUpdateQueue {
             }
         }
 
-        if self.jobs.is_empty() {
-            println!("done");
-            true
-        } else {
-            false
-        }
+        self.jobs.is_empty()
     }
 
     fn balance_edge(&mut self, ctx: &mut ExecuteContext, upper_id: TaskId, task_id: TaskId) {
